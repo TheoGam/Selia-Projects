dba1d61bc235bb6913b1765251a998ebc40f818b
Théo <theogamory@gmail.com>
2018-11-23 15:50:25 +0100

    Définition du css

--- TodoList/Controllers/HomeController.cs
+++ TodoList/Controllers/HomeController.cs
@@ -28,20 +28,6 @@ namespace TodoList.Controllers
             return View(vm);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult AddNewTask()
         {
             return View();
